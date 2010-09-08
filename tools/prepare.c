9c7fbd0d52ff4ae5f78c7952df7c0fc5f0a1fb78
mga83 <mga83@e099a375-04f9-0310-9d5f-a741eaff62e1>
2010-09-08 13:25:51 +0000

    FA sniffer: Fixes to prepare command
    * Add -H option for inspecting existing database
    * Always reset the index area to zero, even if not resetting rest of file
    
    git-svn-id: svn+ssh://serv0002.cs.diamond.ac.uk/home/subversion/repos/controls/diamond/trunk/targetOS/drivers/pcie_sniffer@50063 e099a375-04f9-0310-9d5f-a741eaff62e1

--- tools/prepare.c
+++ tools/prepare.c
@@ -43,15 +43,19 @@ static uint32_t first_decimation = 64;
 static uint32_t second_decimation = 256;
 static double sample_frequency = 10072.4;
 
+static bool read_only = false;
+
 
 static void usage(void)
 {
     printf(
 "Usage: %s [<options>] <capture-mask> <file-name>\n"
+"or:    %s -H <file-name>\n"
 "\n"
 "Prepares or reinitalises a disk file <file-name> for use as an FA sniffer\n"
-"archive.  The given <file-name> can be a block device or an ordinary file.\n"
-"The BPMs specified in <capture-mask> will be captured to disk.\n"
+"archive unless -H is given.  The given <file-name> can be a block device or\n"
+"an ordinary file.  The BPMs specified in <capture-mask> will be captured to\n"
+"disk.\n"
 "\n"
 "The following options can be given:\n"
 "   -s:  Specify size of file.  The file will be resized to the given size\n"
@@ -67,7 +71,9 @@ static void usage(void)
 "File size can be followed by one of K, M, G or T to specify sizes in\n"
 "kilo, mega, giga or terabytes, and similarly block sizes can be followed\n"
 "by one of K or M.\n"
-        , argv0,
+"\n"
+"If instead -H is given then the file header will be printed.\n"
+        , argv0, argv0,
         input_block_size, output_block_size,
         first_decimation, second_decimation,
         sample_frequency);
@@ -125,11 +131,19 @@ static bool process_opts(int *argc, char ***argv)
 
 static bool process_args(int argc, char **argv)
 {
-    return
-        process_opts(&argc, &argv)  &&
-        TEST_OK_(argc == 2, "Try -h for usage")  &&
-        DO_PARSE("capture mask", parse_mask, argv[0], archive_mask)  &&
-        DO_(file_name = argv[1]);
+    if (argc >= 2  &&  strcmp(argv[1], "-H") == 0)
+    {
+        read_only = true;
+        return
+            TEST_OK_(argc == 3, "Try -h for usage")  &&
+            DO_(file_name = argv[2]);
+    }
+    else
+        return
+            process_opts(&argc, &argv)  &&
+            TEST_OK_(argc == 2, "Try -h for usage")  &&
+            DO_PARSE("capture mask", parse_mask, argv[0], archive_mask)  &&
+            DO_(file_name = argv[1]);
 }
 
 
@@ -140,22 +154,35 @@ static bool process_args(int argc, char **argv)
 #define PROGRESS_INTERVAL   16
 
 
-static bool write_new_header(int file_fd)
+/* Resets the index area to zeros, even if the rest of the file is untouched.
+ * This is necessary for a consistent freshly initialised database. */
+static bool reset_index(int file_fd, int index_data_size)
+{
+    void *data_index;
+    bool ok =
+        TEST_NULL(data_index = valloc(index_data_size))  &&
+        DO_(memset(data_index, 0, index_data_size))  &&
+        TEST_write(file_fd, data_index, index_data_size);
+    free(data_index);
+    return ok;
+}
+
+static bool write_new_header(int file_fd, int *written)
 {
     struct disk_header *header;
-    return
+    bool ok =
         TEST_NULL(header = valloc(DISK_HEADER_SIZE))  &&
-        FINALLY(
-            initialise_header(header,
-                archive_mask, file_size,
-                input_block_size, output_block_size,
-                first_decimation, second_decimation, sample_frequency)  &&
-            TEST_IO(lseek(file_fd, 0, SEEK_SET))  &&
-            TEST_write(file_fd, header, DISK_HEADER_SIZE)  &&
-            DO_(print_header(stdout, header)),
-
-            // Finally always do this:
-            DO_(free(header)));
+        initialise_header(header,
+            archive_mask, file_size,
+            input_block_size, output_block_size,
+            first_decimation, second_decimation, sample_frequency)  &&
+        TEST_IO(lseek(file_fd, 0, SEEK_SET))  &&
+        TEST_write(file_fd, header, DISK_HEADER_SIZE)  &&
+        reset_index(file_fd, header->index_data_size)  &&
+        DO_(print_header(stdout, header))  &&
+        DO_(*written = DISK_HEADER_SIZE + header->index_data_size);
+    free(header);
+    return ok;
 }
 
 
@@ -172,14 +199,14 @@ static void show_progress(int n, int final_n)
 }
 
 
-static bool fill_zeros(int file_fd)
+static bool fill_zeros(int file_fd, int written)
 {
     uint32_t block_size = 512*K;
     void *zeros = valloc(block_size);
     memset(zeros, 0, block_size);
 
-    int final_n = file_size / block_size;
-    uint64_t size_left = file_size;
+    uint64_t size_left = file_size - written;
+    int final_n = size_left / block_size;
     bool ok = true;
     for (int n = 0; ok  &&  size_left >= block_size;
          size_left -= block_size, n += 1)
@@ -200,17 +227,30 @@ int main(int argc, char **argv)
         /* For argument errors return 1. */
         return 1;
 
+    bool ok;
     int file_fd;
-    int open_flags = file_size_given ? O_CREAT | O_TRUNC : 0;
-    bool ok =
-        TEST_IO_(file_fd = open(file_name,
-            O_WRONLY | O_DIRECT | open_flags, 0664),
-            "Unable to write to file \"%s\"", file_name)  &&
-        IF_ELSE(file_size_given,
-            fill_zeros(file_fd),
-            get_filesize(file_fd, &file_size))  &&
-        write_new_header(file_fd)  &&
-        TEST_IO(close(file_fd));
+    if (read_only)
+    {
+        char header[DISK_HEADER_SIZE];
+        ok =
+            TEST_IO_(file_fd = open(file_name, O_RDONLY),
+                "Unable to read file \"%s\"", file_name)  &&
+            TEST_read(file_fd, header, DISK_HEADER_SIZE)  &&
+            DO_(print_header(stdout, (struct disk_header *) header));
+    }
+    else
+    {
+        int open_flags = file_size_given ? O_CREAT | O_TRUNC : 0;
+        int written;
+        ok =
+            TEST_IO_(file_fd = open(file_name,
+                O_WRONLY | O_DIRECT | open_flags, 0664),
+                "Unable to write to file \"%s\"", file_name)  &&
+            IF_(!file_size_given, get_filesize(file_fd, &file_size))  &&
+            write_new_header(file_fd, &written)  &&
+            IF_(file_size_given, fill_zeros(file_fd, written))  &&
+            TEST_IO(close(file_fd));
+    }
 
     return ok ? 0 : 2;
 }
